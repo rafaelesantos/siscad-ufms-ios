58184cd2da05a0f22169c4da4915eacab0602ba0
Rafael Escaleira <rafael.escaleira@hotmail.com>
2020-12-02 19:21:21 -0400

    create xcode project

---
+++ Siscad UFMS/Siscad UFMS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Siscad UFMS
+//
+//  Created by Rafael Escaleira on 02/12/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
